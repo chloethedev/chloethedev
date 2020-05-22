6b321266b12b0db6d2070a0f180ad9101109725a
chloethedev <chloethedev@gmail.com>
2020-05-21 22:31:30 -0400

    Started on the Twitter Bot and finally it started working. Seems like still a bug or two tho. Need to fix for later.

--- twitter/eth_btc/index.js
+++ twitter/eth_btc/index.js
@@ -1,19 +1,23 @@
 var ethers = require('ethers');
 //let provider = ethers.getDefaultProvider();
 let etherscanProvider = new ethers.providers.EtherscanProvider();
-let price = 0;
 let newPrice = 0;
+let oldPrice = 0
 
 function getPrice() { 
     etherscanProvider.getEtherPrice().then(function(price) {
         console.log("Ether price in USD: " + price);
-        newPrice = price;
-        console.log("The % change in ETH over the past 60s: " + (price - newPrice)/ price);
+        oldPrice = price;
     });
+       await etherscanProvider.getEtherPrice().then(function(price) {
+        newPrice = price;
+            console.log("The % change in ETH over the past 60s: " + (newPrice - oldPrice) / oldPrice*100 + "%");
+        });
 }
 getPrice()
+setInterval(getPrice, 60000)
 
-setInterval(getPrice, 10000)
+
 
 
 
